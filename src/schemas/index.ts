0242141e42deed12ca5d6ab909fb2582fe970c8d
bl0pez <blopez4434@gmail.com>
2025-01-19 20:34:09 -0300

    Enhance login page with improved UI components and validation for password length

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
 export const loginSchema = yup.object().shape({
-    password: yup.string().required('La contraseña es requerida'),
+    password: yup.string().required('La contraseña es requerida').min(6, 'La contraseña debe tener al menos 6 caracteres'),
     email: yup.string().email('El correo electrónico no es válido').required('El correo electrónico es requerido')
 });
