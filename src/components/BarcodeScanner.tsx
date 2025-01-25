fbdfdc9db478c60defb4931ce97efdedb0e93f8b
bl0pez <blopez4434@gmail.com>
2025-01-25 02:30:46 -0300

    Add react-media-devices for improved media handling in BarcodeScanner

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -10,6 +10,8 @@ import { useEffect, useState } from "react";
 import { useZxing } from "react-zxing";
 import { useCloseTank, useSearchTankByCode } from "../services/tank.service";
 import AnnouncementIcon from '@mui/icons-material/Announcement';
+import { toast } from "react-toastify";
+import { useMediaDevices } from "react-media-devices";
 
 type Action = "barcode" | "manual";
 
@@ -43,9 +45,16 @@ interface ScanProps {
   nextStep: () => void;
 }
 
+const constraints: MediaStreamConstraints = {
+  video: true,
+  audio: false,
+};
+
 function Scan({ result, setResult, nextStep }: ScanProps) {
   const [action, setAction] = useState<Action>("barcode");
   const [paused, setPaused] = useState(false);
+  const { devices } = useMediaDevices({ constraints });
+  const deviceId = devices?.[0]?.deviceId;
 
   async function handleSearch() {
     nextStep();
@@ -53,11 +62,13 @@ function Scan({ result, setResult, nextStep }: ScanProps) {
 
   const { ref } = useZxing({
     onDecodeResult({ getText }) {
+      toast.success(`Código de barras escaneado: ${getText()}`);
       setResult(getText());
       setPaused(true);
       nextStep();
     },
-    paused: paused,
+    paused: !devices,
+    deviceId
   });
 
   useEffect(() => {
